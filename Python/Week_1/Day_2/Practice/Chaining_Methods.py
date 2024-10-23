d3cad7f498f4090c18cf495ee9e18daa3b7bf3f0
helmihamraoui <helmihamraoui54@gmail.com>
2024-10-23 11:57:50 +0100

    Chaining Methods

--- Python/Week_1/Day_2/Practice/Chaining_Methods.py
+++ Python/Week_1/Day_2/Practice/Chaining_Methods.py
@@ -7,25 +7,22 @@ class User :
         self.is_rewards_member=reword
         self.gold_card_points=0
     def display_info(self):
-        print(f"first_name :{self.first_name},\nlast_name:{self.last_name}, \nemail :{self.email}, \nage :{self.age}, \nis_rewards_member :{self.is_rewards_member} , \ngold_card_points: {self.gold_card_points}.")    
+        print(f"first_name :{self.first_name},\nlast_name:{self.last_name}, \nemail :{self.email}, \nage :{self.age}, \nis_rewards_member :{self.is_rewards_member} , \ngold_card_points: {self.gold_card_points}.")   
+        return self
     def enroll(self):
         if (self.is_rewards_member==True):
             print ("User already a member.")
         else:
             self.is_rewards_member=True
             self.gold_card_points=200
+        return self    
     def spend_points(self,amount):
         if(self.gold_card_points>=amount):
             self.gold_card_points=self.gold_card_points-amount
         else:
-            print("you don't have  points")    
+            print("you don't have  points")
+        return self        
 user1=User("helmi","hamaraoui","helmihamraoui54@gmail.com",20) 
-user1.display_info()
-user1.enroll()
-user1.spend_points(50)
 user2=User("ahmed","marweni","ahmedmr@gmail.com",22) 
-user2.display_info()
-user2.enroll()
-user2.spend_points(80)
-user3=User("yassin","jlasi","yassin158@gmail.com",24) 
-user3.spend_points(40)
+user1.display_info().enroll().spend_points(50).display_info()
+user2.enroll().spend_points(80).display_info()
