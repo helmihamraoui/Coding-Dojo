d1890a6a39e8ae8c631fba41df57736bad6ed2e0
helmihamraoui <helmihamraoui54@gmail.com>
2024-10-24 17:43:39 +0100

    dojo pets

--- Python/Week_1/Day_3/Practice/Dojo_Pets/Dojo_Pets.py
+++ Python/Week_1/Day_3/Practice/Dojo_Pets/Dojo_Pets.py
@@ -1,5 +1,5 @@
 from Ninja import*
-from Python.Week_1.Day_3.Practice.Dojo_Pets.Dojo_pet.Pets import*
+from Pets import*
 ninja1=Ninja('mouhamed','ali','cat','50','fruit')
 ninja2=Ninja('ali','ismail','dog','50','cat')
 my_pet=Pet('mimi','mousse','set','150','100')
