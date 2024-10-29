0990431785faca91196a0cfe0bb38009e55651f3
helmihamraoui <helmihamraoui54@gmail.com>
2024-10-29 13:11:48 +0100

    basketball

--- Python/Week_1/Day_4/Practice/Checkerboard/serveur.py
+++ Python/Week_1/Day_4/Practice/Checkerboard/serveur.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html", rows=8, cols=8, color1="black", color2="white")
+    return render_template("index.html", rows=8, cols=8, color1="red", color2="black")
 
 @app.route('/<int:x>')
 def checkerboard_rows(x):
-    return render_template("index.html", rows=x, cols=8, color1="black", color2="white")
+    return render_template("index.html", rows=x, cols=8, color1="red", color2="black")
 
 @app.route('/<int:x>/<int:y>')
 def checkerboard_custom(x, y):
-    return render_template("index.html", rows=x, cols=y, color1="black", color2="white")
+    return render_template("index.html", rows=x, cols=y, color1="red", color2="black")
 
 @app.route('/<int:x>/<int:y>/<color1>/<color2>')
 def checkerboard_colors(x, y, color1, color2):
