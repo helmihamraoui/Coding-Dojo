98be5933b5dacf0f23ad2592b25a6d3b934670d2
helmihamraoui <helmihamraoui54@gmail.com>
2024-11-06 13:10:14 +0100

    login

---
+++ Python/Week_3/Day_1/Core/login_regisrtion/flask_app/controllers/user_controller.py
@@ -0,0 +1,42 @@
+from flask_app import app
+from flask import render_template,redirect,request,session,flash
+from flask_app.models.user_model import User
+from flask_bcrypt import Bcrypt
+bcrypt=Bcrypt(app)
+@app.route('/')
+def registration():
+    return render_template('index.html')
+@app.route('/register/create',methods=['post'])
+def create_new():
+    if User.validate(request.form):
+        pw=bcrypt.generate_password_hash(request.form['password'])
+        data={
+            **request.form,
+            "password":pw
+        }
+        user_id=User.register(data)
+        session["user_id"]=user_id
+        return redirect('/home')
+    else:
+        return redirect('/')
+@app.route('/login',methods=['POST'])
+def login():
+    user=User.get_one({'email':request.form['email']})
+    if not user :
+        flash("Invalide Email/password",'login')
+        return redirect('/')
+    if not bcrypt.check_password_hash(user.password,request.form['password']):
+        flash("Invalide Email/password",'login')
+        return redirect('/')
+    session['user_id']=user.id
+    return redirect('/home')
+@app.route('/home')
+def home():
+    if  'user_id' in session:
+        return render_template('home.html')
+    else:
+        return redirect('/')
+@app.route("/logout",methods=['POST'])
+def logout():
+    session.clear()
+    return redirect('/')
