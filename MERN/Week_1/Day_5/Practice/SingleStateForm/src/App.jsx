efb6b786711ca74c5f417ebcf988c559fe3f2351
helmihamraoui <helmihamraoui54@gmail.com>
2024-12-01 20:35:02 +0100

    react

---
+++ MERN/Week_1/Day_5/Practice/SingleStateForm/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Form from './components/Form.jsx';
+import './App.css';
+
+function App() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+});
+  return (
+    <div className="container mt-5">
+      <Form setFormData={setFormData} formData={formData}/>
+      <div className="mt-4 p-3 border rounded bg-light shadow">
+            <ul className="list-group">
+            <li className="list-group-item">
+                <strong>First Name:</strong> {formData.firstName}
+            </li>
+            <li className="list-group-item">
+                <strong>Last Name:</strong> {formData.lastName}
+            </li>
+            <li className="list-group-item">
+                <strong>Email:</strong> {formData.email}
+            </li>
+            <li className="list-group-item">
+                <strong>Password:</strong> {formData.password}
+            </li>
+            <li className="list-group-item">
+                <strong>Confirm Password:</strong> {formData.confirmPassword}
+                </li>
+            </ul>
+        </div>
+    </div>
+  );
+}
+
+export default App;
